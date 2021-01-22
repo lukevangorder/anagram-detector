ea51c5a79d8b10b4ce429ebb8c890a7909f4a653
lukevangorder <lukevangorder@gmail.com>
2021-01-22 13:30:09 -0500

    commit tree 3a81ff30276631b11359afbe686a536d00515b4c

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+    def match(possible)
+        @letters = @word.split("")
+        send_back = []
+        possible.each do |check|
+            if check.split("").sort == @letters.sort
+                send_back.push(check)
+            end
+        end
+        send_back
+    end
+    attr_accessor :word
+end
